01f17da2cf05a5a8518f1b1fe4b63657a64fe118
so1eeee <soljeong821@gmail.com>
2024-03-06 14:20:45 +0900

    test commit

---
+++ src/test/java/sofTodo/toDoList/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package sofTodo.toDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
