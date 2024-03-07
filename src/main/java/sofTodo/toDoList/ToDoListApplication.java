358970cde88aa7a780a7a8d408b59679fb8cdaff
so1eeee <soljeong821@gmail.com>
2024-03-07 15:28:22 +0900

    done

--- src/main/java/sofTodo/toDoList/ToDoListApplication.java
+++ src/main/java/sofTodo/toDoList/ToDoListApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ToDoListApplication {
 	public static void main(String[] args) {
-		System.out.println("a");
 		SpringApplication.run(ToDoListApplication.class, args);
 	}
 }
