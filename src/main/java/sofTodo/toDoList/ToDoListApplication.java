c010afe6723e3bce3d0de656d1606dbcf4338616
so1eeee <soljeong821@gmail.com>
2024-03-07 15:30:58 +0900

    branch develop

--- src/main/java/sofTodo/toDoList/ToDoListApplication.java
+++ src/main/java/sofTodo/toDoList/ToDoListApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ToDoListApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Branch develop");
 		SpringApplication.run(ToDoListApplication.class, args);
 	}
 
