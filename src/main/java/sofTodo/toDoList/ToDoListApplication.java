709b2e3f2c357083b01e3ce3aeb207b128074a7c
so1eeee <soljeong821@gmail.com>
2024-03-11 21:05:48 +0900

    rebase test

--- src/main/java/sofTodo/toDoList/ToDoListApplication.java
+++ src/main/java/sofTodo/toDoList/ToDoListApplication.java
@@ -9,6 +9,7 @@ public class ToDoListApplication {
 	public static void main(String[] args) {
 		System.out.println("Branch develop");
 		System.out.println("jaebin");
+		System.out.println("Rebase Test");
 		SpringApplication.run(ToDoListApplication.class, args);
 	}
 
