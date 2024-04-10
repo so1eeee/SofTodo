d26c5d15c7904e14ef23b6f854f976884b95f0b7
so1eeee <soljeong821@gmail.com>
2024-04-10 15:35:17 +0900

    update

--- src/main/java/sofTodo/toDoList/controller/MemberController.java
+++ src/main/java/sofTodo/toDoList/controller/MemberController.java
@@ -2,16 +2,33 @@ package sofTodo.toDoList.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import sofTodo.toDoList.service.MemberService;
 
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
+
     @GetMapping("/join")
     public String register() {return "join.html";}
 
+    @PostMapping("/addmember")
+    public String addMember(String username, String password, String nickname) {
+        memberService.addMember(username, password, nickname);
+        return "redirect:/memberlist";
+    }
+
+    @GetMapping("/memberlist")
+    String list(Model model) {
+        memberService.findAll(model);
+        return "memberlist";
+    }
+
     @GetMapping("/login")
     public String login() {
-        return "login.html";
+        return "login";
     }
 }
--- src/main/java/sofTodo/toDoList/service/MemberService.java
+++ src/main/java/sofTodo/toDoList/service/MemberService.java
@@ -1,4 +1,8 @@
 package sofTodo.toDoList.service;
 
+import org.springframework.ui.Model;
+
 public interface MemberService {
+    void addMember(String username, String password, String nickname);
+    void findAll(Model model);
 }
