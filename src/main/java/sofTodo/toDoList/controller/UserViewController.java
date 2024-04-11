e30c2557ee4d6442a5fe71784783bb83aecb7a1a
so1eeee <soljeong821@gmail.com>
2024-04-11 21:55:20 +0900

    login view

--- src/main/java/sofTodo/toDoList/controller/UserViewController.java
+++ src/main/java/sofTodo/toDoList/controller/UserViewController.java
@@ -2,12 +2,19 @@ package sofTodo.toDoList.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserViewController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    // 우선 로그인 성공하면 todolist View 로 return 처리 해줬습니다.
+    @PostMapping("/login")
+    public String loginSuccess(){
+        return "todolist";
     }
 
     @GetMapping("/signup")
