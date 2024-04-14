9c128e27d02d59fcfa52324dbd0eeffa858a9061
so1eeee <soljeong821@gmail.com>
2024-04-14 15:39:55 +0900

    search done

--- src/main/java/sofTodo/toDoList/config/WebSecurityConfig.java
+++ src/main/java/sofTodo/toDoList/config/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -20,6 +21,11 @@ import sofTodo.toDoList.service.UserDetailService;
 public class WebSecurityConfig {
     private final UserDetailService userService;
 
+    @Bean
+    public WebSecurityCustomizer configure(){
+        return (web) -> web.ignoring()
+                .requestMatchers(new AntPathRequestMatcher("/static/**"));
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.csrf((csrf) -> csrf.disable());
--- src/main/java/sofTodo/toDoList/controller/UserApiController.java
+++ src/main/java/sofTodo/toDoList/controller/UserApiController.java
@@ -4,15 +4,19 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import sofTodo.toDoList.dto.AddUserRequest;
+import sofTodo.toDoList.repository.UserRepository;
 import sofTodo.toDoList.service.UserService;
 
 @RequiredArgsConstructor
 @Controller
 public class UserApiController {
     private final UserService userService;
+    private final UserRepository userRepository;
 
     @PostMapping("/user")
     public String signup(AddUserRequest request) {
@@ -24,5 +28,11 @@ public class UserApiController {
     public String logout(HttpServletRequest request, HttpServletResponse response) {
         userService.logout(request, response);
         return "redirect:/login";
+    }
+
+    @GetMapping("/search")
+    String doSearch(@RequestParam String searchUser, Model model){
+        userService.doSearch(searchUser, model);
+        return "memberlist";
     }
 }
