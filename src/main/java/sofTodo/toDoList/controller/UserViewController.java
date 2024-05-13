e262d8c5078d95c914cfbb467f8286170e2cef92
jaebinGit <tnwoql327@gmail.com>
2024-05-13 15:40:13 +0900

    feat : OAuth, form login 구현3

--- src/main/java/sofTodo/toDoList/controller/UserViewController.java
+++ src/main/java/sofTodo/toDoList/controller/UserViewController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UserViewController {
     @GetMapping("/login")
     public String login() {
-        return "oauthlogin";
+        return "login";
     }
 
     @GetMapping("/signup")
