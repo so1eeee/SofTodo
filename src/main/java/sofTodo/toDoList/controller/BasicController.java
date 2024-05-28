aa88539f4f91e3f830259158e741b6e85bf9ef19
so1eeee <soljeong821@gmail.com>
2024-05-28 16:51:53 +0900

    BasicController에 ranking메서드 수정

--- src/main/java/sofTodo/toDoList/controller/BasicController.java
+++ src/main/java/sofTodo/toDoList/controller/BasicController.java
@@ -3,11 +3,19 @@ package sofTodo.toDoList.controller;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import sofTodo.toDoList.service.UserService;
 
 @Controller
 public class BasicController {
+    private final UserService userService;
+
+    public BasicController(UserService service) {
+        userService = service;
+    }
+
     //로그인을 한 유저가 "/" URL로 접속을 하면 /home으로 redirect 해줌! 아니라면? -> hello(첫 시작 페이지) 페이지로..
     @GetMapping("/")
     public ModelAndView homepage() {
@@ -18,9 +26,9 @@ public class BasicController {
         return new ModelAndView("hello");
     }
 
-
     @GetMapping("/ranking")
-    public String ranking(){
+    public String ranking(Model model) {
+        model.addAttribute("topUsers", userService.getTop5UsersByMissionSuccessCount());
         return "ranking";
     }
 
