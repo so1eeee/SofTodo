5f01cf818dad461caf9bee8211eb0da084d97097
jaebinGit <tnwoql327@gmail.com>
2024-05-29 13:17:53 +0900

    feat : OAtuh user와 form user의 principal 객체가 서로 다른 것으로 확인, 두 객체가 동일한 효과를 내게 하기 위하여 OAuth user를 커스터마이징

---
+++ src/main/java/sofTodo/toDoList/config/oauth/CustomOAuth2User.java
@@ -0,0 +1,70 @@
+package sofTodo.toDoList.config.oauth;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import sofTodo.toDoList.domain.User;
+
+import java.util.Collection;
+import java.util.Map;
+
+@Getter
+public class CustomOAuth2User implements OAuth2User, UserDetails {
+    private final OAuth2User oAuth2User;
+    private final User user;
+    private final String nickname;
+    private Long missionSuccessCount;
+
+    public CustomOAuth2User(OAuth2User oAuth2User, User user) {
+        this.oAuth2User = oAuth2User;
+        this.user = user;
+        this.nickname = user.getNickname();// nickname 설정
+        this.missionSuccessCount = user.getMissionSuccessCount();
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return oAuth2User.getAttributes();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getAuthorities();
+    }
+
+    @Override
+    public String getName() {
+        return oAuth2User.getName();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return user.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return user.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return user.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
+}
