7074f7ec06f6fe14c36ad30c08bac0045071dd3f
jaebinGit <tnwoql327@gmail.com>
2024-06-10 10:02:19 +0900

    refactor : 본인이 쓴 방명록만 수정 삭제 버튼 보이게

--- src/main/java/sofTodo/toDoList/dto/GuestBookResponse.java
+++ src/main/java/sofTodo/toDoList/dto/GuestBookResponse.java
@@ -8,12 +8,14 @@ public class GuestBookResponse {
     private Long id;
     private String content;
     private String date;
+    private Long userId; // guestUser의 ID
     private String nickname;
 
     public GuestBookResponse(GuestBook guestBook) {
         this.id = guestBook.getId();  // id 필드 추가
         this.content = guestBook.getContent();
         this.date = guestBook.getDate();
+        this.userId = guestBook.getGuestUser().getId(); // guestUser의 ID 설정
         this.nickname = guestBook.getGuestUser().getNickname();
     }
 }
